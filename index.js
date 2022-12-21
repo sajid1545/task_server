9df77a5ebeb9040d31c335990317d4e51d29dcc6
sajid9962 <clashking1545@gmail.com>
2022-12-21 20:30:02 +0600

    Email system implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -18,7 +19,6 @@ const client = new MongoClient(uri, {
 	serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
 	try {
 		const infoCollection = client.db('Task').collection('info');
@@ -28,6 +28,44 @@ async function run() {
 
 			const result = await infoCollection.insertOne(info);
 			res.send(result);
+		});
+
+		app.get('/info', async (req, res) => {
+			const query = {};
+			const information = await infoCollection.find(query).toArray();
+			res.send(information);
+		});
+
+		app.post('/send-email', async (req, res) => {
+			const info = req.body;
+			const transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: 'clashking1545@gmail.com',
+					pass: process.env.NODE_MAIL_PASSWORD,
+				},
+			});
+
+			const mailOptions = {
+				from: 'hello@example.com',
+				to: 'sajjadabdullah9962@gmail.com',
+				subject: 'Redpositive Service Task',
+				text: `
+                Email - ${info.Email}
+                Name - ${info.Name}
+                Phone - ${info.Phone}
+                Hobbies - ${info.Hobbies}
+                `,
+			};
+
+			transporter.sendMail(mailOptions, function (error, info) {
+				if (error) {
+					console.log(error);
+				} else {
+					console.log('Email sent: ' + info.response);
+					// do something useful
+				}
+			});
 		});
 	} finally {
 	}
